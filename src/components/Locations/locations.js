10cbd42e7e0e5075834b4abed24ceeb6236dd5b7
Kún Ákos <kunaakos@gmail.com>
2016-12-15 20:47:05 +0100

    hacked layout quite a bit, added some shadows

--- src/components/Locations/locations.js
+++ src/components/Locations/locations.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import template from './locations.html';
 
+import './locations.scss';
+
 import LocationSearch from 'components/LocationSearch/locationsearch';
 import LocationCard from 'components/LocationCard/locationcard';
 
@@ -19,19 +21,47 @@ export default Vue.extend({
 
   data() {
     return {
-      localOrder: []
+      dragging: false,
+      scrolledEnough: false,
+      stickySize: 0,
+      localOrder: [],
+      dangerZone: []
     };
   },
 
-  computed: mapGetters({
-    order: 'card_g__order',
-    cards: 'card_g__cards'
-  }),
+  computed: {
+    hasScrolledEnough() {
+      return this.scrolledEnough;
+    },
+
+    ...mapGetters({
+      order: 'card_g__order',
+      cards: 'card_g__cards'
+    })
+  },
 
   methods: {
-    setOrder() {
-      // commit the modified card order array
+    dragStart() {
+      this.dragging = true;
+    },
+
+    dragEnd() {
+      this.dragging = false;
       this.$store.dispatch('card__order_set', this.localOrder);
+      // TODO: shouldn't dispatch if element was deleted, since it's not visible locally, going to be removed from remote automatically
+    },
+
+    dangerZoneAdd(event) {
+      // card was AADDDED TOOO THHEE DANGERR ZONE ... erm we delete it
+      this.$store.dispatch('card__cards_delete', this.dangerZone[event.newIndex]);
+    },
+
+    checkScroll(event) {
+      if (event.target.scrollTop >= 42) {
+        this.scrolledEnough = true;
+      } else if (this.scrolledEnough) {
+        this.scrolledEnough = false;
+      }
     }
   },
 
@@ -45,5 +75,8 @@ export default Vue.extend({
     this.$watch('order', function(newVal) {
       self.localOrder = newVal.slice();
     });
+
+    this.stickySize = this.$el.querySelector('.sticky').clientHeight;
+    // console.log(this.$el.querySelector('.sticky'));
   }
 });
